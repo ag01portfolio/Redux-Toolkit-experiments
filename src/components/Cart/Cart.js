6d18ef093af94c1453396cf81cb8ec7f2ca0f168
Abhi - ROG <ag01portfolio@gmail.com>
2025-08-23 23:26:20 +0530

    Add Cart component and update styles in index.css

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,9 @@
+import Card from "../UI/Card";
+import classes from "./Cart.module.css";
+import CartItem from "./CartItem";
+
+const Cart = (props) => {
+  return <Card className={classes.cart}></Card>;
+};
+
+export default Cart;
